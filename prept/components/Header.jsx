cefcc2c49e701ffbb3661d59d66352f4e2f00401
Gopal Chavan <chavangopal811@gmail.com>
2026-04-28 10:23:10 +0530

    Prept

---
+++ prept/components/Header.jsx
@@ -0,0 +1,86 @@
+// import { checkUser } from "@/lib/checkUser";
+import { Button } from "./ui/button";
+// import { SignIn,signOut } from "@clerk/nextjs";
+import {
+  SignIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+  Show,
+} from "@clerk/nextjs";
+import Link from "next/link";
+import Image from "next/image";
+// import RoleRedirect from "./RoleRedirect";
+// import CreditButton from "./CreditButton";
+import { CalendarDays, Users } from "lucide-react";
+
+const Header = async () => {
+  //   const user = await checkUser();
+
+  return (
+    <nav className="fixed top-0 inset-x-0 z-50 flex items-center justify-between px-3 sm:px-10 py-3 border-b border-white/7 backdrop-blur-xl">
+     
+      <Link href="/">
+        <Image
+          src="/logo.png"
+          alt="Prept Logo"
+          width={100}
+          height={100}
+          className="h-11 w-auto"
+        />
+      </Link>
+      {/* {user && <RoleRedirect role={user.role} />} */}
+      <div className="flex items-center gap-3">
+        <Show when="signed-out">
+          <SignInButton >
+            <Button variant="ghost">Sign in</Button>
+          </SignInButton>
+          <SignInButton >
+            <Button variant="gold">Get started →</Button>
+          </SignInButton>
+        </Show>
+        <Show when="signed-in">
+          <UserButton />
+        </Show>
+
+        {/* <SignedIn>
+          {user?.role === "INTERVIEWER" && (
+            <Button variant="ghost" asChild>
+              <Link href="/dashboard">Dashboard</Link>
+            </Button>
+          )}
+
+          {user?.role === "INTERVIEWEE" && (
+            <>
+              <Button variant="ghost" asChild>
+                <Link href="/explore">
+                  <Users size={16} />
+                  <span className="hidden md:inline">Explore</span>
+                </Link>
+              </Button>
+              <Button variant="default" asChild>
+                <Link href="/appointments">
+                  <CalendarDays size={16} />
+                  <span className="hidden md:inline">My Appointments</span>
+                </Link>
+              </Button>
+            </>
+          )}
+
+          <CreditButton
+            role={user?.role === "INTERVIEWER" ? "INTERVIEWER" : "INTERVIEWEE"}
+            credits={
+              (user?.role === "INTERVIEWER"
+                ? user?.creditBalance
+                : user?.credits) ?? 0
+            }
+          />
+
+          <UserButton />
+        </SignedIn> */}
+      </div>
+    </nav>
+  );
+};
+
+export default Header;//Ganeshya@08112002
